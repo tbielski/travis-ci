ddc9bf0015bcdbf1e02c6c18a18f4f29bd818208
Tymoteusz Bielski <tymekb@onet.pl>
2021-03-09 11:41:33 +0100

    test1

---
+++ sample.test.js
@@ -0,0 +1,23 @@
+// const request = require('supertest')
+
+// const app = require('./index.js')
+
+// describe('Post Endpoints', () => {
+//   it('should create a new post', async () => {
+//     const res = await request(app)
+//       .post('/')
+//       .send({
+//         userId: 1,
+//         title: 'test is cool',
+//       })
+//     expect(res.statusCode).toEqual(201)
+//     expect(res.body).toHaveProperty('post')
+//   })
+// })
+
+describe('Sample Test', () => {
+    it('should test that true === true', () => {
+      expect(true).toBe(true)
+    })
+  })
+  
